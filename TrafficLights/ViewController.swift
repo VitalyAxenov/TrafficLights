173a23198fe12b675f728cd088fcd015454031b5
Vitaly Axenov <hrmobs@gmail.com>
2022-01-13 02:41:47 +0300

    Code for first press

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var redView: UIView!
+    
+    @IBOutlet var yellowView: UIView!
+    
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func lightSwitcher() {
+        if button.title(for: .normal) == "Start" {
+            button.setTitle("Next", for: .normal)
+            redView.alpha = 1
+        }
+        
+    }
+    
 }
 

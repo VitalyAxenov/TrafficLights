355592d8d9f47efdb3762f9525390d999d15b676
Vitaly Axenov <hrmobs@gmail.com>
2022-01-13 22:21:05 +0300

    Final design

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -21,16 +21,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func lightSwitching() {
-        
-//        if button.title(for: .normal) == "Start" {
-//            button.setTitle("Next", for: .normal)
-//            redView.alpha = 1
-//          return
-//        }
 
         if redView.alpha == 1 {
             redView.alpha = 0.3
@@ -49,34 +42,11 @@ class ViewController: UIViewController {
             redView.alpha = 1
             return
         }
+        
         button.setTitle("Next", for: .normal)
         redView.alpha = 1
     }
-    
-    /*
-    @IBAction func lightSwitcher() {
-        
-      if button.title(for: .normal) == "Start" {
-            button.setTitle("Next", for: .normal)
-            redView.alpha = 1
-        }
 
-        if redView.alpha == 1 {
-            redView.alpha = 0.3
-            yellowView.alpha = 1
-        }
-
-        if yellowView.alpha == 1 {
-            yellowView.alpha = 0.3
-            greenView.alpha = 1
-        }
-
-        if greenView.alpha == 1 {
-            greenView.alpha = 0.3
-            redView.alpha = 1
-        }
-
-    } */
 }
     
 
